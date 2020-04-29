b6fcc42b06ee03e29becd5ccd4ed3974ead99267
unocelli <unocelli@gmail.com>
2020-04-29 20:49:54 +0200

    Warning of unauthorized, Pipe property layout

--- client/src/app/auth.guard.ts
+++ client/src/app/auth.guard.ts
@@ -2,10 +2,14 @@ import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from
 import { Injectable } from '@angular/core';
 import { AuthService } from './_services/auth.service';
 import { ProjectService } from './_services/project.service';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
     constructor(private authService: AuthService,
+        private translateService: TranslateService,
+        private toastr: ToastrService,
         private projectService: ProjectService, 
         private router: Router) { }
 
@@ -20,6 +24,17 @@ export class AuthGuard implements CanActivate {
         }
 
         // this.router.navigate(['/home']);
+        this.notifySaveError();
         return false;
+    }
+
+    private notifySaveError() {
+        let msg = '';
+        this.translateService.get('msg.signin-failed').subscribe((txt: string) => { msg = txt });
+        this.toastr.error(msg, '', {
+            timeOut: 3000,
+            closeButton: true,
+            disableTimeOut: true
+        });
     }
 }
