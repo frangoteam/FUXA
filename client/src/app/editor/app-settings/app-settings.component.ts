fddd1d1bffc9dcfad6cb3e8eec58f66c1f093a2a
unocelli <unocelli@gmail.com>
2021-04-28 20:12:58 +0200

    Chinese translation thanks zjjczzs

--- client/src/app/editor/app-settings/app-settings.component.ts
+++ client/src/app/editor/app-settings/app-settings.component.ts
@@ -13,7 +13,7 @@ import { AppSettings } from '../../_models/settings';
 })
 export class AppSettingsComponent implements OnInit {
 
-    languageType = [ { text: 'dlg.app-language-en', value: 'en' }, { text: 'dlg.app-language-ru', value: 'ru' }, { text: 'dlg.app-language-ua', value: 'ua' } ];
+    languageType = [ { text: 'dlg.app-language-en', value: 'en' }, { text: 'dlg.app-language-ru', value: 'ru' }, { text: 'dlg.app-language-ua', value: 'ua' }, { text: 'dlg.app-language-zh-cn', value: 'zh-cn' } ];
     authType = [ { text: 'dlg.app-auth-disabled', value: '' }, { text: 'dlg.app-auth-expiration-15m', value: '15m' }, 
                         { text: 'dlg.app-auth-expiration-1h', value: '1h' }, { text: 'dlg.app-auth-expiration-3h', value: '3h' },
                         { text: 'dlg.app-auth-expiration-1d', value: '1d' }];
