19a64fabc0b897372b4e28b9aa184f9c89618336
unocelli <unocelli@gmail.com>
2021-05-20 07:56:43 +0200

    FUXA client

---
+++ client/src/environments/environment.client.ts
@@ -0,0 +1,9 @@
+declare function require(moduleName: string): any;
+export const environment = {
+  version: require('../../package.json').version,
+  production: false,
+  apiEndpoint: null,
+  apiPort: 1881,
+  serverEnabled: false,
+  type: 'client'
+};
