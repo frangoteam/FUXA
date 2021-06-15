89732987314ceb50dbbb4aa833f61dbb35247893
unocelli <unocelli@gmail.com>
2021-06-15 21:27:01 +0200

    Partial Turkish translation

--- client/src/app/editor/app-settings/app-settings.component.ts
+++ client/src/app/editor/app-settings/app-settings.component.ts
@@ -15,7 +15,7 @@ export class AppSettingsComponent implements OnInit {
 
     languageType = [ { text: 'dlg.app-language-en', value: 'en' }, { text: 'dlg.app-language-ru', value: 'ru' },
                      { text: 'dlg.app-language-ua', value: 'ua' }, { text: 'dlg.app-language-zh-cn', value: 'zh-cn' },
-                     { text: 'dlg.app-language-pt', value: 'pt' } ];
+                     { text: 'dlg.app-language-pt', value: 'pt' }, { text: 'dlg.app-language-tr', value: 'tr' }];
     authType = [ { text: 'dlg.app-auth-disabled', value: '' }, { text: 'dlg.app-auth-expiration-15m', value: '15m' }, 
                         { text: 'dlg.app-auth-expiration-1h', value: '1h' }, { text: 'dlg.app-auth-expiration-3h', value: '3h' },
                         { text: 'dlg.app-auth-expiration-1d', value: '1d' }];
