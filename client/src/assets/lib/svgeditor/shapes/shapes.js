7eab63301e86c2051222eb6da050ae0168581b69
Ángel Fernández Sobrino <fernandezsobrinoangel@gmail.com>
2022-05-24 10:49:09 +0200

    Add shapes loading script update
    Both from backend and frontend

--- client/src/assets/lib/svgeditor/shapes/shapes.js
+++ client/src/assets/lib/svgeditor/shapes/shapes.js
@@ -10,32 +10,52 @@ var svgedit = svgedit || {};
     $.getJSON('/assets/shapes/shapes.json', function (data) {
         var shapesToLoad = data.shapesToLoad;
 
-        svgedit.shapes.load = function (path, callback) {
-            var progress = 0;
-            $.each(shapesToLoad, function() {
-                var name = this;
-                $.getJSON('/assets/shapes/' + name + '.json', function(data) {
-                    for (var i = 0; i < data.shapes.length; i++) {
-                        data.shapes[i].name = data.typeId + '-' + data.shapes[i].name;
-                    }
-                    if (svgEditor.shapesGrps[data.shapesGroupName]) {
+        $.getJSON('http://localhost:1881/api/resources/shapes', function (data) {
+            svgedit.shapes.load = function (path, callback) {
+                var progress = 0;
+                $.each(data.groups, function() {
+                    $.getJSON(`http://localhost:1881/api/resources/json-shapes/${this.name}`, function(data) {
                         for (var i = 0; i < data.shapes.length; i++) {
-                            svgEditor.shapesGrps[data.shapesGroupName].push(data.shapes[i]);
+                            data.shapes[i].name = data.typeId + '-' + data.shapes[i].name;
                         }
-                    } else {
-                        svgEditor.shapesGrps[data.shapesGroupName] = data.shapes;
-                    }
-                    if (++progress == shapesToLoad.length) {
-                        callback();
-                    }
-                }).fail(function(){
-                    console.log('ERROR: loading ' + '/assets/shapes/' + name  + '.json');
-                    if (++progress == shapesToLoad.length) {
-                        callback();
-                    }
+                        if (svgEditor.shapesGrps[data.shapesGroupName]) {
+                            for (var i = 0; i < data.shapes.length; i++) {
+                                svgEditor.shapesGrps[data.shapesGroupName].push(data.shapes.shapes[i]);
+                            }
+                        } else {
+                            svgEditor.shapesGrps[data.shapesGroupName] = data.shapes;
+                        }
+                    }).fail(function(){
+                        console.log('ERROR: loading ' + '/assets/shapes/' + name  + '.json');
+                    });
                 });
-            });
-            return true;
-        };
+
+                $.each(shapesToLoad, function() {
+                    var name = this;
+                    $.getJSON('/assets/shapes/' + name + '.json', function(data) {
+                        for (var i = 0; i < data.shapes.length; i++) {
+                            data.shapes[i].name = data.typeId + '-' + data.shapes[i].name;
+                        }
+                        if (svgEditor.shapesGrps[data.shapesGroupName]) {
+                            for (var i = 0; i < data.shapes.length; i++) {
+                                svgEditor.shapesGrps[data.shapesGroupName].push(data.shapes[i]);
+                            }
+                        } else {
+                            svgEditor.shapesGrps[data.shapesGroupName] = data.shapes;
+                        }
+                        if (++progress == shapesToLoad.length) {
+                            callback();
+                        }
+                    }).fail(function(){
+                        console.log('ERROR: loading ' + '/assets/shapes/' + name  + '.json');
+                        if (++progress == shapesToLoad.length) {
+                            callback();
+                        }
+                    });
+                });
+
+                return true;
+            };
+        });
     });
 }());
