f674bd4321b15bea9768ace6a16144b98807b134
unocelli <unocelli@gmail.com>
2022-12-11 20:57:55 +0100

    merge and lint

--- client/src/app/editor/card-config/card-config.component.spec.ts
+++ client/src/app/editor/card-config/card-config.component.spec.ts
@@ -1,7 +1,5 @@
 /* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { CardConfigComponent } from './card-config.component';
 
--- client/src/app/gauges/gauge-property/flex-auth/flex-auth.component.spec.ts
+++ client/src/app/gauges/gauge-property/flex-auth/flex-auth.component.spec.ts
@@ -1,7 +1,5 @@
 /* tslint:disable:no-unused-variable */
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { FlexAuthComponent } from './flex-auth.component';
 
