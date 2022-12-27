45e41fa8ee81aba71bcac6452a3487d66b706c4c
unocelli <unocelli@gmail.com>
2022-12-27 16:13:16 +0100

    fixed influxdb missing timestamp

--- server/runtime/devices/device-utils.js
+++ server/runtime/devices/device-utils.js
@@ -4,7 +4,7 @@ module.exports = {
 
     tagDaqToSave: function (tag, timestamp) {
         if (tag.daq && tag.daq.enabled) {
-            const a = timestamp - parseInt(tag.daq.interval) * 1000;
+            tag.timestamp = timestamp;
             if (tag.changed && tag.daq.changed) {
                 return true;
             } else if (!tag.daq.lastDaqSaved || timestamp - parseInt(tag.daq.interval) * 1000 > tag.daq.lastDaqSaved) {
