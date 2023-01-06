c3f4f6308aa9006e9b5a3c1a8b62371b0f0820e1
unocelli <unocelli@gmail.com>
2023-01-06 18:17:12 +0100

    fixed input on number (removed sign validator)

--- client/src/app/_directives/number.directive.ts
+++ client/src/app/_directives/number.directive.ts
@@ -4,14 +4,13 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
     selector: '[numberOnly]'
 })
 export class NumberOnlyDirective {
-    // Allow decimal numbers and negative values
-    private regex = new RegExp(/^-?[0-9]+(\.[0-9]*){0,1}$/g);///^-?[0-9]+(\.[0-9]*){0,1}$/g);
     // Allow key codes for special events. Reflect :
     // Backspace, tab, end, home
     private specialKeys: Array<string> = ['Backspace', 'Delete', 'Tab', 'End', 'Home', 'ArrowLeft', 'ArrowRight'];
 
     constructor(private el: ElementRef) {
     }
+
     @HostListener('keydown', ['$event'])
     onKeyDown(event: KeyboardEvent) {
         // Allow Backspace, tab, end, and home keys
@@ -19,24 +18,6 @@ export class NumberOnlyDirective {
             event.stopPropagation();
             return;
         }
-        let current: string = this.el.nativeElement.value;
-        let next = '';
-        if (event.key === '-') {
-            event.preventDefault();
-            if (!current.startsWith('-')) {
-                next = event.key + current;
-                this.el.nativeElement.value = next;
-            }
-        } else {
-            next = current.concat(event.key);
-        }
-        if (next && !String(next).match(this.regex)) {
-            event.preventDefault();
-            if (this.el.nativeElement.value === '') {
-                this.el.nativeElement.value = '1';
-            }
-        }
-        event.stopPropagation();
     }
 }
 
