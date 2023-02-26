8e7c139dcd9be7263d833ccb1a6e10b990994fd9
ABykiev <bykiev.andrei@mail.ru>
2023-02-26 20:47:52 +0300

    Update Angular

--- client/src/polyfills.ts
+++ client/src/polyfills.ts
@@ -18,12 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
 
 
 /***************************************************************************************************
