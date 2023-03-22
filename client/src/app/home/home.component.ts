a5f819a09ebbb92739d9de3beb3494115535b237
Mauro Alexandre <mauro@webrota.com.br>
2023-03-22 14:41:37 -0300

    remove debug

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -137,7 +137,6 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     }
 
     onGoToPage(viewId: string, force: boolean = false) {
-        debugger;
         if (viewId === this.viewAsAlarms) {
             this.onAlarmsShowMode('expand');
             this.checkToCloseSideNav();
