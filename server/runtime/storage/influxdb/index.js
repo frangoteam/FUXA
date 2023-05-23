d400f58818bc55c480372f769cf1d1c8a8a3692e
unocelli <unocelli@gmail.com>
2023-05-23 21:30:16 +0200

    WIP

--- server/runtime/storage/influxdb/index.js
+++ server/runtime/storage/influxdb/index.js
@@ -5,11 +5,10 @@
 "use strict";
 
 var utils = require('../../utils');
-
+let influx = require('influx')
 var { InfluxDB, Point, flux } = require('@influxdata/influxdb-client');
+//var { InfluxDB, Point, flux } = ***@***.***/influxdb-client');
 
-const VERSION_18_FLUX = '1.8-flux';
-const VERSION_20 = '2.0';
 
 function Influx(_settings, _log) {
 
@@ -17,36 +16,32 @@ function Influx(_settings, _log) {
     var logger = _log;                      // Application logger
     var status = InfluxDBStatusEnum.CLOSE;
 
-    var hostname = '';
-    var port = 8086;
-    var database = '';
-    var name = '';
 
     var influxError = { error: null, timestamp: 0 };
     var influxdbVersion = VERSION_20;
     var client = null;
-    var clientOptions = null;
+    var clientOptions = null
     var writeApi = null
     var queryApi = null
 
     this.init = function () {
 
-        influxdbVersion = settings.daqstore.version;
-        if (!influxdbVersion) {
-            influxdbVersion = VERSION_20;
-        }
+        // This is where it needs to have a FRONT END so this options is dynamic//----------------------------------->>>
 
-        if (settings.daqstore.credentials && (influxdbVersion === VERSION_18_FLUX || influxdbVersion === VERSION_20)) {
-
-            const token = influxdbVersion === VERSION_18_FLUX ?
-                `${settings.daqstore.credentials.username}:${settings.daqstore.credentials.password}` :
-                settings.daqstore.credentials.token;
+        if (settings.daqstore.url != "") {
 
             clientOptions = {
-                url: settings.daqstore.url,
-                // rejectUnauthorized: n.rejectUnauthorized,
-                token
+                host: "the host or the IP of influxserver (string)",
+                port: 8086,
+                protocol: 'https', // *could be http or https
+                database: "the database name in the influxdb (string)",
+                username:'username (string)',
+                password:'password (string)',
+                options: {
+                    rejectUnauthorized: false // this options is needed when protocol is set to https and the influxdb server is using a self-signed certificate (it is important)
+                }
             }
+            // client = new influx.InfluxDB(clientOptions);
             client = new InfluxDB(clientOptions);
             writeApi = client.getWriteApi(settings.daqstore.organization, settings.daqstore.bucket, 's');
             queryApi = client.getQueryApi(settings.daqstore.organization);
@@ -57,7 +52,7 @@ function Influx(_settings, _log) {
     this.close = function () {
         try {
             status = InfluxDBStatusEnum.CLOSE;
-            writeApi.close().then(() => {
+            client.close.then(() => {
                 logger.info('influxdb-close FINISHED');
             })
             .catch((e) => {
@@ -78,18 +73,32 @@ function Influx(_settings, _log) {
         logger.error('influxdb-addDaqValue Not supported!');
     }
 
-    this.addDaqValues = function (tags, deviceName) {
-        for (var tagid in tags) {
-            if (!tags[tagid].daq || !tags[tagid].daq.enabled) {
+    this.addDaqValues = function (tagsValues, deviceName) {
+        var dataToWrite = []
+        for (var tagid in tagsValues) {
+            let tag = tagsValues[tagid];
+            if (!tag.daq || !tag.daq.enabled || utils.isNullOrUndefined(tag.value)) {
                 continue;
             }
-            writePoint(tags[tagid], deviceName);
+            const tags = {
+                id: tag.id,
+                devicename: deviceName
+            }
+            let fields = {
+                value: tag.value
+            }
+            if (utils.isBoolean(tag.value)) {
+                fields.value = tag.value * 1
+            }
+            var pts = {
+                measurement: tag.name,
+                tags,
+                fields,
+                timestamp: tag.timestamp * 1000000 || new Date().getTime() * 1000000
+            }
+            dataToWrite.push(pts)
         }
-        writeApi.flush(true).then(() => {
-            // reset last error ;
-        }).catch(error => {
-            setError(error);
-        });
+        writePoints(dataToWrite)
     }
 
     this.getDaqMap = function (tagid) {
@@ -100,7 +109,7 @@ function Influx(_settings, _log) {
 
     this.getDaqValue = function (tagid, fromts, tots) {
         return new Promise(function (resolve, reject) {
-            const query = flux`from(bucket: "${settings.daqstore.bucket}") |> range(start: ${new Date(fromts)}, stop: ${new Date(tots)}) |> filter(fn: (r) => r._measurement == "${tagid}")`;
+            const query = flux`from(bucket: "${settings.daqstore.bucket}") |> range(start: ${new Date(fromts)}, stop: ${new Date(tots)}) |> filter(fn: (r) => r.id == "${tagid}")`;
             try {
                 var result = [];
                 queryApi.queryRows(query, {
@@ -123,23 +132,9 @@ function Influx(_settings, _log) {
         });
     }
 
-    function writePoint(tag, deviceName) {
+    async function writePoints(pts) {
         try {
-            if (!utils.isNullOrUndefined(tag.value)) {
-                const point = new Point(tag.id)
-                .tag('name', tag.name)
-                .tag('type', tag.type)
-                .timestamp(new Date(tag.timestamp || new Date().getTime()));
-                if (deviceName) {
-                    point.tag('device', deviceName);
-                }
-                if (utils.isBoolean(tag.value)) {
-                    point.booleanField('value', tag.value);
-                } else {
-                    point.floatField('value', tag.value)
-                }
-                writeApi.writePoint(point);
-            }
+            await writeApi.writePoints(pts);
         } catch (error) {
             logger.error(`influxdb-writePoints failed! ${error}`);
         }
