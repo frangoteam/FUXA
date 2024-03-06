9a74112f99ad7b283daeea12d0395e21b7ee9c6d
unocelli <4frango@gmail.com>
2024-03-06 21:38:34 +0100

    feat: refactoring script client mode and add sys function

--- client/src/app/_services/script.service.ts
+++ client/src/app/_services/script.service.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs';
 import { EndPointApi } from '../_helpers/endpointapi';
 import { environment } from '../../environments/environment';
 import { Script, ScriptMode } from '../_models/script';
+import { ProjectService } from './project.service';
+import { HmiService } from './hmi.service';
 
 @Injectable({
     providedIn: 'root'
@@ -13,11 +15,14 @@ export class ScriptService {
 
     private endPointConfig: string = EndPointApi.getURL();
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient,
+                private projectService: ProjectService,
+                private hmiService: HmiService) {
+
+    }
 
     runScript(script: Script) {
         return new Observable((observer) => {
-
             if (!script.mode || script.mode == ScriptMode.SERVER) {
                 if (environment.serverEnabled) {
                     let header = new HttpHeaders({ 'Content-Type': 'application/json' });
@@ -32,11 +37,40 @@ export class ScriptService {
                     observer.next();
                 }
             } else {
-                if (script.parameters && script.parameters.length > 0) {
+                if (script.parameters?.length > 0) {
                     console.warn('TODO: Script with mode CLIENT not work with parameters.');
                 }
-                eval(script.code);
+                try {
+                    let code = script.code.replace(/\$getTag\(/g, 'this.$getTag(');
+                    code = code.replace(/\$setTag\(/g, 'this.$setTag(');
+                    const result = eval(code);
+                    observer.next(result);
+                } catch (err) {
+                    console.error(err);
+                    observer.error(err);
+                }
             }
         });
+    }
+
+    evalScript(script: Script) {
+        if (script.parameters?.length > 0) {
+            console.warn('TODO: Script with mode CLIENT not work with parameters.');
+        }
+        try {
+            let code = script.code.replace(/\$getTag\(/g, 'this.$getTag(');
+            code = code.replace(/\$setTag\(/g, 'this.$setTag(');
+            eval(code);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    public $getTag(id: string) {
+        return this.projectService.getTagFromId(id)?.value;
+    }
+
+    public $setTag(id: string, value: any) {
+        const tag = this.hmiService.putSignalValue(id, value);
     }
 }
