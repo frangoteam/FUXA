7f9910412dea54474250ad18ae6a7375749b4b66
rikugun <v5.rikugun@gmail.com>
2024-03-12 21:34:17 +0800

    rollback environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,7 +2,7 @@ declare function require(moduleName: string): any;
 export const environment = {
   version: require('../../package.json').version,
   production: false,
-  apiEndpoint: "http://192.168.100.60:1881",
+  apiEndpoint: null,
   apiPort: 1881,
   serverEnabled: true,
   type: null
