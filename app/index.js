dc171a2adcf1d41469578d461be68da065db9425
rikugun <v5.rikugun@gmail.com>
2024-04-24 18:07:37 +0800

    feat: change for test

--- app/index.js
+++ app/index.js
@@ -1,22 +1,47 @@
-const {app, BrowserWindow} = require('electron')
+const {app, utilityProcess, BrowserWindow} = require('electron')
 const path = require('node:path')
 
-const fs = require('node:fs')
+const fs = require('fs')
+const {fork} = require('child_process');
+
 let serverEntry = path.join(__dirname, 'server/main.js')
 if (!fs.existsSync(serverEntry)) {
     serverEntry = path.join(__dirname, '../server/main.js')
 }
+const appData = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/ApplicationSupport' : process.env.HOME + "/.local/share")
+const userDataDir = path.join(appData, 'fuxa-app', 'data')
+
+if (!fs.existsSync(userDataDir)) {
+    fs.mkdirSync(userDataDir)
+    // fs.copyFileSync(path.join(__dirname, 'settings.default.js'),path.join(userDataDir,'settings.default.js'))
+}
+//setting for fuxa server
+process.env.userDir = userDataDir
+
 require(serverEntry)
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1024,
         height: 768,
+        webPreferences: {
+            // preload: path.join(__dirname, 'preload.js')
+        }
     })
     //wait for server startup
-    setTimeout(()=>{
+    setTimeout(() => {
         win.loadURL('http://localhost:1881')
-    },1000)
+    }, 1000)
+
+    // const worker = new BrowserWindow({
+    //     show:true,
+    //     width: 1024,
+    //     height: 768,
+    //     webPreferences: {
+    //         nodeIntegration: true
+    //     }
+    // })
+    // worker.loadFile(path.join(__dirname,'server.html'))
 }
 
 app.whenReady().then(() => {
