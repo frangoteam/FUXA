67e74eaa8653260005a8456c43ed158052cc019c
rikugun <rikugun@ymail.com>
2024-04-25 08:47:08 +0800

    feat:  add multi-platform support; support old osx version

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ let serverEntry = path.join(__dirname, 'server/main.js')
 if (!fs.existsSync(serverEntry)) {
     serverEntry = path.join(__dirname, '../server/main.js')
 }
-const appData = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/ApplicationSupport' : process.env.HOME + "/.local/share")
+const appData = process.env.APPDATA || (process.platform == 'darwin' ? process.env.HOME + '/Library/Application Support' : process.env.HOME + "/.local/share")
 const userDataDir = path.join(appData, 'fuxa-app', 'data')
 
 if (!fs.existsSync(userDataDir)) {
