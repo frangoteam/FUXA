1f65150732490b81e8a950ce1ea5b885482dfcfb
Greg Carter <greg9504@gmail.com>
2024-05-20 09:42:24 -0400

    clean up changes around scale scripts

--- client/src/app/device/tag-options/tag-options.component.ts
+++ client/src/app/device/tag-options/tag-options.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit } from '@angular/core';
-import { FormArray, FormGroup, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FuxaServer, TagDaq, TagScale, TagScaleModeType } from '../../_models/device';
-import { Utils, jsonValidator } from '../../_helpers/utils';
+import { Utils} from '../../_helpers/utils';
 import { ProjectService } from '../../_services/project.service';
 import { Subscription } from 'rxjs';
 import { Script, ScriptMode, ScriptParam, ScriptParamType } from '../../_models/script';
@@ -21,8 +21,6 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
     formGroup: UntypedFormGroup;
     scaleModeType = TagScaleModeType;
     scripts: Script[];
-    selectedReadScript: Script;
-    selectedWriteScript: Script;
     configedReadParams: {[key: string]: ScriptAndParam[]} = {};
     configedWriteParams: {[key: string]: ScriptParam[]} = {};
 
@@ -54,9 +52,7 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
             dateTimeFormat: null,
             scaleRead: null,
             scaleReadFunction: null,
-            scaleReadParams: this.fb.array([]),
             scaleWriteFunction: null,
-            scaleWriteParams: null,
         });
 
         this.formGroup.controls.enabled.valueChanges.subscribe(enabled => {
@@ -83,9 +79,9 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
             let scaledHigh = { value: null, valid: true };
             let dateTimeFormat = { value: null, valid: true };
             let scaleReadFunction = { value: null, valid: true };
-            let scaleReadParams = { value: [], valid: true };
+            //let scaleReadParams = { value: [], valid: true };
             let scaleWriteFunction = { value: null, valid: true };
-            let scaleWriteParams = { value: null, valid: true };
+            //let scaleWriteParams = { value: null, valid: true };
             for (let i = 0; i < this.data.tags.length; i++) {
                 if (!this.data.tags[i].daq) {
                     continue;
@@ -131,7 +127,7 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
                 }
                 //if (scaleReadParams.value.length === 0) {
                     //scaleReadParams.value = this.data.tags[i].scaleReadParams;
-                    const script = this.scripts.find(s => s.id === this.data.tags[i].scaleReadFunction);
+                    let script = this.scripts.find(s => s.id === this.data.tags[i].scaleReadFunction);
                     if (this.data.tags[i].scaleReadParams) {
                         const tagParams = JSON.parse(this.data.tags[i].scaleReadParams) as ScriptParam[];
                         const notValid = this.initializeScriptParams(script, tagParams, this.configedReadParams);
@@ -143,17 +139,17 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
                 if (!scaleWriteFunction.value) {
                     scaleWriteFunction.value = this.data.tags[i].scaleWriteFunction;
                 }
-                if (!scaleWriteParams.value) {
-                    scaleWriteParams.value = this.data.tags[i].scaleWriteParams;
-                    const script = this.scripts.find(s => s.id === this.data.tags[i].scaleWriteFunction);
+                //if (!scaleWriteParams.value) {
+                //    scaleWriteParams.value = this.data.tags[i].scaleWriteParams;
+                    script = this.scripts.find(s => s.id === this.data.tags[i].scaleWriteFunction);
                     if (this.data.tags[i].scaleWriteParams) {
                         const tagParams = JSON.parse(this.data.tags[i].scaleWriteParams) as ScriptParam[];
                         const notValid = this.initializeScriptParams(script, tagParams, this.configedWriteParams);
-                        if (notValid) {
-                            scaleWriteParams.value = undefined;
-                        }
+                        //if (notValid) {
+                        //    scaleWriteParams.value = undefined;
+                        //}
                     }
-                }
+                //}
             }
             let values = {};
             if (enabled.valid && enabled.value !== null) {
@@ -184,25 +180,21 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
             if (scaleReadFunction.valid && scaleReadFunction.value) {
                 values = {...values, scaleReadFunction: scaleReadFunction.value};
             }
-            if (scaleReadParams.valid && scaleReadParams.value) {
-                values = {...values, scaleReadParams: scaleReadParams.value};
-            }
+            // if (scaleReadParams.valid && scaleReadParams.value) {
+            //     values = {...values, scaleReadParams: scaleReadParams.value};
+            // }
             if (scaleWriteFunction.valid && scaleWriteFunction.value) {
                 values = {...values, scaleWriteFunction: scaleWriteFunction.value};
             }
-            if (scaleWriteParams.valid && scaleWriteParams.value) {
-                values = {...values, scaleWriteParams: scaleWriteParams.value};
-            }
+            // if (scaleWriteParams.valid && scaleWriteParams.value) {
+            //     values = {...values, scaleWriteParams: scaleWriteParams.value};
+            // }
             this.formGroup.patchValue(values);
-            //this.loadParamsFormArray(this.configedReadParams, this.scaleReadParams);
-            //this.formGroup.setControl('scaleRead', this.initScripsFC('readscripts', this.configedReadParams));
             if (this.data.device?.id === FuxaServer.id) {
                 this.formGroup.controls.scaleMode.disable();
             }
             this.formGroup.updateValueAndValidity();
             this.onCheckScaleMode(this.formGroup.value.scaleMode);
-            this.onReadScriptChanged(this.formGroup.value.scaleReadFunction);
-            this.onWriteScriptChanged(this.formGroup.value.scaleWriteFunction);
         }
 
         this.formGroup.controls.scaleMode.valueChanges.subscribe(value => {
@@ -250,15 +242,6 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
         } else {
             readParamsStr = undefined;
         }
-
-        // const readParams = [];
-        // for (const p of this.scaleReadParams.controls) {
-        //     if (p.value.scriptId === this.formGroup.value.scaleReadFunction) {
-        //         const param: ScriptParam = new ScriptParam(p.get('name').value, ScriptParamType.value);
-        //         readParams.push(param);
-        //     }
-        // }
-        // const readParamsStr = readParams.length > 0 ? JSON.stringify(readParams) : undefined;
         let writeParamsStr;
         if (this.configedWriteParams[this.formGroup.value.scaleWriteFunction]) {
             writeParamsStr = JSON.stringify(this.configedWriteParams[this.formGroup.value.scaleWriteFunction]);
@@ -308,84 +291,7 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
         }
         return false;
     }
-    onReadScriptChanged(scriptId) {
-        console.log(`Selected script ${scriptId}`);
-       // this.enablePramsArray(this.scaleReadParams, scriptId);
-        // if (scriptId) {
-        //     this.formGroup.controls.scaleReadParams.setValidators(jsonValidator);
-        //     this.selectedReadScript = this.scripts.find(s => s.id === scriptId);
-        // } else {
-        //     this.formGroup.controls.scaleReadParams.clearValidators();
-        //     this.selectedReadScript = undefined;
-        // }
-        this.formGroup.updateValueAndValidity();
-    }
-    onWriteScriptChanged(scriptId) {
-        console.log(`Selected script            ${scriptId}`);
-        if (scriptId) {
-            this.formGroup.controls.scaleWriteParams.setValidators(jsonValidator);
-            this.selectedWriteScript = this.scripts.find(s => s.id === scriptId);
-        } else {
-            this.formGroup.controls.scaleWriteParams.clearValidators();
-            this.selectedWriteScript = undefined;
-        }
-        this.formGroup.updateValueAndValidity();
-    }
 
-    // get readScripts() {
-    //     return this.formGroup.get('scaleRead') as FormArray;
-    // }
-
-    // getReadParametersFormArray(scriptIndex: number) {
-    //     return this.readScripts.controls[scriptIndex].get('parameters') as FormArray;
-    //   }
-
-    //   getReadParameterControls(scriptIndex: number) {
-    //     return this.getReadParametersFormArray(scriptIndex).controls;
-    //   }
-
-    //   addReadScript(script: Script) {
-    //     this.readScripts.push(this.getScriptItem(script.name, script.id));
-    //   }
-    //   addReadParameter(scriptIndex: number, name: string, value: any) {
-    //     this.getReadParametersFormArray(scriptIndex).push(this.getParameterItem(name, value));
-    //   }
-    //   private addScriptFC(scriptsFCA: FormArray, script: Script) {
-    //     scriptsFCA.push(this.getParameterItem())
-
-    //   }
-    // private initScripsFC(name: string, defaultScriptParams: {[key: string]: ScriptAndParam[]}): FormGroup {
-    //     const fb = this.fb.group({
-    //       name: name,
-    //       scripts: this.fb.array([])
-    //     });
-    //     const fbsArray = fb.get('scripts') as FormArray;
-    //     for (const script of this.scripts) {
-    //         //this.addScriptFC(fb.get('scripts') as FormArray, script);
-    //         const sfg = this.getScriptItem(script.name, script.id);
-    //         const sfgPArray = sfg.get('parameters') as FormArray;
-    //         for (const p of defaultScriptParams[script.id]) {
-    //             const pfg = this.getParameterItem(p.name, p.value);
-    //             sfgPArray.push(pfg);
-    //         }
-    //         fbsArray.push(sfg);
-    //     }
-    //     return fb;
-    // }
-    // private getScriptItem(name: string, id: string) {
-    //     return this.fb.group({
-    //       name: name,
-    //       id: id,
-    //       parameters: this.fb.array([])
-    //     });
-    //   }
-
-    //   private getParameterItem(name: string = undefined, value: any = undefined) {
-    //     return this.fb.group({
-    //       name: name,
-    //       value:[value, Validators.required]
-    //     });
-    //   }
     private loadScripts() {
         //scripts that can be used to scale a tag must have the first parameter named "value" of
         // type value and be
@@ -421,46 +327,7 @@ export class TagOptionsComponent implements OnInit, OnDestroy {
         }
         this.scripts = filteredScripts;
 	}
-    // get scaleReadParams() {
-    //     return this.formGroup.get('scaleReadParams') as FormArray;
-    // }
-    // private addReadParam(scriptId: string, name: string, value: any) {
-    //     const param = this.fb.group( {
-    //         scriptId: [scriptId],
-    //         name: [name],
-    //         value: [value,[Validators.required]]
-    //     });
-    //     this.scaleReadParams.push(param);
-    // }
-    // private addParamToFormArray(paramsArray: FormArray, scriptId: string, name: string, value: any) {
-    //     const param = this.fb.group( {
-    //         scriptId: [scriptId],
-    //         name: [name],
-    //         value: [value,[Validators.required]]
-    //     });
-    //     paramsArray.push(param);
-    // }
-    // private enablePramsArray(paramsArray: FormArray, scriptId: string) {
-    //     for (const fcParam of paramsArray.controls) {
-    //         if (fcParam.value.scriptId === scriptId) {
-    //             fcParam.enable;
-    //         } else {
-    //             fcParam.disable;
-    //         }
-    //     }
-    // }
-    // private enableParamsForSelectedScripts(allParamsForAllScripts: FormArray) {
-    //     this.enablePramsArray(allParamsForAllScripts, this.formGroup.value.scaleReadFunction);
-    // }
-    // private loadParamsFormArray(defaultScriptParams: {[key: string]: ScriptAndParam[]}, paramsArray: FormArray) {
-    //     //defaultScriptParams now updated with passed in values, build formarray from this
-    //     for (const key in defaultScriptParams) {
-    //         const sp = defaultScriptParams[key];
-    //         for (const p of sp) {
-    //             this.addParamToFormArray(paramsArray, p.scriptId, p.name, p.value);
-    //         }
-    //     }
-    // }
+
     /**
      * updates this.configedParams list with the previously saved list of param/values for
      * the script.
