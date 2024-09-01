8c0e9e5b57c36b2ec16f01f90cbc56c91af4ac50
jw <jaywu.jjwu@gmail.com>
2024-09-01 18:48:07 +0800

    Build for production by J

--- client/dist/assets/lib/svgeditor/shapes/shapes.js
+++ client/dist/assets/lib/svgeditor/shapes/shapes.js
@@ -8,7 +8,7 @@ var svgedit = svgedit || {};
     }
 
     // Add here the your shapes file name 
-    var shapesToLoad = ['my-shapes.js', 'proc-shapes.js', 'ape-shapes.js'];
+    var shapesToLoad = ['my-shapes.js', 'proc-shapes.js', 'ape-shapes.js', 'meta-shapes.js'];
 
     svgedit.shapes.load = function (path, callback) {
         var progress = 0;
