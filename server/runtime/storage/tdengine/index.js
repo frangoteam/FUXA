60d7b346e16bc1b7166a8a1367d131e92f9777dd
jw <jaywu.jjwu@gmail.com>
2024-09-06 13:38:26 +0800

    install axios and load alarms from sqlite db to tdengine

--- server/runtime/storage/tdengine/index.js
+++ server/runtime/storage/tdengine/index.js
@@ -1,7 +1,7 @@
 'use strict'
-
-let {options, connect} = require("@tdengine/rest");
+let { options, connect } = require("@tdengine/rest");
 let utils = require('../../utils');
+const axios = require('axios');
 
 function TDengine(_settings, _log, _currentStorage) {
     let settings = _settings;               // Application settings
@@ -16,6 +16,7 @@ function TDengine(_settings, _log, _currentStorage) {
     }
     var fncGetTagProp = null;
 
+
     this.init = async function () {
         let connOpt = Object.assign({}, options, settings.daqstore)
         connOpt.user = settings.daqstore.credentials.username;
@@ -27,12 +28,146 @@ function TDengine(_settings, _log, _currentStorage) {
             //TODO add retention
             let res = await cursor.query(`CREATE DATABASE IF NOT EXISTS ${database} `);
             res = await cursor.query(`CREATE STABLE IF NOT EXISTS ${database}.meters (dt TIMESTAMP,tag_id VARCHAR(200), tag_value BINARY(20)) TAGS (device_id VARCHAR(200),device_name BINARY(256) )`);
-
+            //add by J, create alarm stable
+            res = await cursor.query(`CREATE TABLE IF NOT EXISTS ${database}.alarms (alm_ts TIMESTAMP, alm_ontime BIGINT, alm_offtime BIGINT, alm_acktime BIGINT, alm_name NCHAR(255), alm_type NCHAR(255), alm_status NCHAR(255), alm_text NCHAR(255), alm_userack NCHAR(255),alm_group NCHAR(255),alm_bkcolor NCHAR(255), alm_color NCHAR(255))`);
         } catch (error) {
             console.error(error);
         }
 
+        //add by J, to save alarm data from Sqlite to tdEngine-------------------------------------------------------------------------------
+
+        // const dataArrayAlm = [
+        //     {
+        //         "ontime": 1725462862675,
+        //         "offtime": 1725462865659,
+        //         "acktime": 1725462871434,
+        //         "name": "serverTag AlarmNum",
+        //         "type": "highhigh",
+        //         "status": "NF",
+        //         "text": "this is HH-alarm of serverTag1 number",
+        //         "userack": "",
+        //         "group": "Server",
+        //         "bkcolor": "#FF4848",
+        //         "color": "#FFF"
+        //     },
+        //     {
+        //         "ontime": 1725460256618,
+        //         "offtime": 0,
+        //         "acktime": 0,
+        //         "name": "serverTag AlarmBit",
+        //         "type": "high",
+        //         "status": "N",
+        //         "text": "this is H-alarm of serverTag1 bit",
+        //         "userack": "",
+        //         "group": "Server",
+        //         "bkcolor": "#F9CF59",
+        //         "color": "#000"
+        //     },
+        //     {
+        //         "ontime": 1725459152258,
+        //         "offtime": 1725460244613,
+        //         "acktime": 1725460252091,
+        //         "name": "serverTag AlarmBit",
+        //         "type": "high",
+        //         "status": "NF",
+        //         "text": "this is H-alarm of serverTag1 bit",
+        //         "userack": "",
+        //         "group": "Server",
+        //         "bkcolor": "#F9CF59",
+        //         "color": "#000"
+        //     },
+        //     {
+        //         "ontime": 1725458298760,
+        //         "offtime": 1725458302762,
+        //         "acktime": 1725458305126,
+        //         "name": "serverTag AlarmBit",
+        //         "type": "high",
+        //         "status": "NF",
+        //         "text": "this is H-alarm of serverTag1 bit",
+        //         "userack": "",
+        //         "group": "Server",
+        //         "bkcolor": "#F9CF59",
+        //         "color": "#000"
+        //     },
+        //     {
+        //         "ontime": 1725458286754,
+        //         "offtime": 1725458292756,
+        //         "acktime": 1725458296969,
+        //         "name": "serverTag AlarmBit",
+        //         "type": "high",
+        //         "status": "NF",
+        //         "text": "this is H-alarm of serverTag1 bit",
+        //         "userack": "",
+        //         "group": "Server",
+        //         "bkcolor": "#F9CF59",
+        //         "color": "#000"
+        //     },
+        //     {
+        //         "ontime": 1725458272747,
+        //         "offtime": 1725458283752,
+        //         "acktime": 1725458284890,
+        //         "name": "serverTag AlarmNum",
+        //         "type": "highhigh",
+        //         "status": "NF",
+        //         "text": "this is HH-alarm of serverTag1 number",
+        //         "userack": "",
+        //         "group": "Server",
+        //         "bkcolor": "#FF4848",
+        //         "color": "#FFF"
+        //     },
+        //     {
+        //         "ontime": 1725458249739,
+        //         "offtime": 1725458258739,
+        //         "acktime": 1725458262808,
+        //         "name": "serverTag AlarmNum",
+        //         "type": "highhigh",
+        //         "status": "NF",
+        //         "text": "this is HH-alarm of serverTag1 number",
+        //         "userack": "",
+        //         "group": "Server",
+        //         "bkcolor": "#FF4848",
+        //         "color": "#FFF"
+        //     }
+        // ];
+
+
+        // Function to insert data into the tdEngine database
+        async function insertAlarms(dataArray) {
+            for (let i = 0; i < dataArray.length; i++) {
+                const item = dataArray[i];
+                const queryAlm = `INSERT INTO ${database}.alarms (alm_ts, alm_ontime, alm_offtime, alm_acktime, alm_name, alm_type, alm_status, alm_text, alm_userack, alm_group, alm_bkcolor, alm_color)
+                VALUES (
+                    NOW(), ${item.ontime}, ${item.offtime}, ${item.acktime}, '${item.name}', '${item.type}', '${item.status}', '${item.text}', '${item.userack}', '${item.group}', '${item.bkcolor}', '${item.color}'
+                )`;
+
+                try {
+                    await cursor.query(queryAlm);
+                    console.log(`Inserted item ${i + 1} successfully.`);
+                } catch (err) {
+                    console.error(`Failed to insert item ${i + 1}. Error:`, err);
+                }
+            }
+            console.log('All items processed.');
+        }
+
+        // Define the dataArray variable to store data retrieved from the API
+        let dataArrayAlm = [];
+
+        // axios to get hisotry alarm data from Alarm API
+        axios.get('http://docker002:1881/api/alarmshistory')
+            .then(response => {
+                dataArrayAlm = response.data;
+                // console.log('Data retrieved:', dataArrayAlm);
+                insertAlarms(dataArrayAlm);
+            })
+            .catch(error => {
+                console.error('Error retrieving data:', error);
+            });
+
+        //----------------------------------------------------------------------------------------------------
+
     }
+
 
     this.addDaqValues = function (tagsValues, deviceName, deviceId) {
         var dataToRestore = [];
@@ -41,7 +176,7 @@ function TDengine(_settings, _log, _currentStorage) {
             let tag = tagsValues[tagid];
             if (!tag.daq || utils.isNullOrUndefined(tag.value) || Number.isNaN(tag.value)) {
                 if (tag.daq.restored) {
-                    dataToRestore.push({id: tag.id, deviceId: deviceId, value: tag.value});
+                    dataToRestore.push({ id: tag.id, deviceId: deviceId, value: tag.value });
                 }
                 if (!tag.daq.enabled) {
                     continue;
@@ -69,14 +204,14 @@ function TDengine(_settings, _log, _currentStorage) {
     this.getDaqValue = function (tagid, fromts, tots) {
         return new Promise(function (resolve, reject) {
             const cursor = conn.cursor()
-            let data =[]
+            let data = []
             cursor.query(`SELECT CAST(dt as BIGINT) as dt, tag_value
                           FROM ${database}.meters
                           WHERE dt >= ${fromts}
                             and dt < ${tots} `).then((result) => {
                 // logger.debug(result)
-                result.getData().forEach((row) =>{
-                    data.push({dt: row[0],value: row[1]})
+                result.getData().forEach((row) => {
+                    data.push({ dt: row[0], value: row[1] })
                 })
                 resolve(data)
             }).catch((error) => {
@@ -96,7 +231,12 @@ function TDengine(_settings, _log, _currentStorage) {
         return dummy;
     }
 
+
+
     this.init().then(() => logger.info("TDengine connected"))
+
+
+
 }
 
 module.exports = {
