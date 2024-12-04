5635ba6cc762c06c710998a188b19342739a2b28
Kiruthick-Roxx <129523146+Kiruthick-Roxx@users.noreply.github.com>
2024-12-04 15:58:59 +0530

    Update chart.ts

--- client/src/app/_models/chart.ts
+++ client/src/app/_models/chart.ts
@@ -14,6 +14,13 @@ export class ChartLine {
     fill?: string;
     yaxis: number;
     lineInterpolation?: number;
+    strokezones?: ChartLineZone[];
+    fillzones?: ChartLineZone[];
+}
+export interface ChartLineZone {
+    min: number;
+    max: number;
+    color: string;
 }
 
 export enum ChartViewType {
