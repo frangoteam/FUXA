24e7ac3a4c12309135f649f1cd5e11cdbc51ef19
unocelli <unocelli@gmail.com>
2025-01-26 17:35:21 +0100

    feat: icons filter in layout

--- client/src/app/editor/layout-property/layout-header-item-property/layout-header-item-property.component.ts
+++ client/src/app/editor/layout-property/layout-header-item-property/layout-header-item-property.component.ts
@@ -1,7 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
 import { HeaderItem, HeaderItemType } from '../../../_models/hmi';
-import { Observable, map, of } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, map, of } from 'rxjs';
 import { ProjectService } from '../../../_services/project.service';
 import { Define } from '../../../_helpers/define';
 import { Utils } from '../../../_helpers/utils';
@@ -14,6 +14,9 @@ import { Utils } from '../../../_helpers/utils';
 export class LayoutHeaderItemPropertyComponent {
     item: HeaderItem;
     icons$: Observable<string[]>;
+    filteredIcons$: Observable<string[]>;
+    filterText = '';
+    private filterTextSubject = new BehaviorSubject<string>('');
     headerType = <HeaderItemType[]>['button', 'label', 'image'];
     defaultColor = Utils.defaultColor;
 
@@ -21,11 +24,20 @@ export class LayoutHeaderItemPropertyComponent {
         public projectService: ProjectService,
         public dialogRef: MatDialogRef<LayoutHeaderItemPropertyComponent>,
         @Inject(MAT_DIALOG_DATA) public data: HeaderItem) {
+
         this.item = data;
         this.icons$ = of(Define.MaterialIconsRegular).pipe(
             map((data: string) => data.split('\n')),
             map(lines => lines.map(line => line.split(' ')[0])),
             map(names => names.filter(name => !!name))
+        );
+        this.filteredIcons$ = combineLatest([
+            this.icons$,
+            this.filterTextSubject.asObservable()
+        ]).pipe(
+            map(([icons, filterText]) =>
+                icons.filter(icon => icon.toLowerCase().includes(filterText.toLowerCase()))
+            )
         );
     }
 
@@ -35,5 +47,9 @@ export class LayoutHeaderItemPropertyComponent {
 
     onOkClick(): void {
         this.dialogRef.close(this.item);
+    }
+
+    onFilterChange() {
+        this.filterTextSubject.next(this.filterText);
     }
 }
