84dddad59811e2cc25bbb26020f3a6e2c79642c2
unocelli <unocelli@gmail.com>
2025-03-16 22:23:51 +0100

    fix: try touch for production

--- client/src/app/gui-helpers/ngx-uplot/ngx-uplot.component.ts
+++ client/src/app/gui-helpers/ngx-uplot/ngx-uplot.component.ts
@@ -239,12 +239,12 @@ export class NgxUplotComponent implements OnInit, OnDestroy {
         // }
         // set plugins
         opt.plugins = (this.options.tooltip && this.options.tooltip.show) ? [this.tooltipPlugin()] : [];
-
-        this.uplot = new uPlot(opt, this.data, this.graph.nativeElement);
         if (this.options.thouchZoom) {
             console.log('touch zoom');
             opt.plugins.push(this.touchZoomPlugin({}));
         }
+        this.uplot = new uPlot(opt, this.data, this.graph.nativeElement);
+
     }
 
     setOptions(options: Options) {
@@ -599,6 +599,7 @@ export class NgxUplotComponent implements OnInit, OnDestroy {
             }
 
             function touchmove(e) {
+                console.log('touchmove');
                 storePos(to, e);
 
                 if (!rafPending) {
@@ -608,6 +609,7 @@ export class NgxUplotComponent implements OnInit, OnDestroy {
             }
 
             over.addEventListener('touchstart', function(e) {
+                console.log('touchstart');
                 rect = over.getBoundingClientRect();
 
                 storePos(fr, e);
@@ -625,6 +627,7 @@ export class NgxUplotComponent implements OnInit, OnDestroy {
             });
 
             over.addEventListener('touchend', function(e) {
+                console.log('touchend');
                 document.removeEventListener('touchmove', touchmove, {});
             });
         }
