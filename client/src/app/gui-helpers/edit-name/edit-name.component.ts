5cde3a4cf5e51bbee6b326b884398b9dcdeaee74
unocelli <unocelli@gmail.com>
2025-10-03 11:44:54 +0200

    fix: View property and diverse

--- client/src/app/gui-helpers/edit-name/edit-name.component.ts
+++ client/src/app/gui-helpers/edit-name/edit-name.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialogRef as MatDialogRef, MAT_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
     selector: 'app-edit-name',
