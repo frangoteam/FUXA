7d60378c90c0e30df4168e99975fe0c88985d896
unocelli <unocelli@gmail.com>
2025-10-03 13:06:38 +0200

    fix: editor header

--- client/src/app/header/header.component.ts
+++ client/src/app/header/header.component.ts
@@ -23,7 +23,7 @@ const saveFromEditorRouteKey = ['/device', '/messages', '/language', '/users', '
 @Component({
     selector: 'app-header',
     templateUrl: 'header.component.html',
-    styleUrls: ['header.component.css']
+    styleUrls: ['header.component.scss']
 })
 export class HeaderComponent implements AfterViewInit, OnDestroy {
 
