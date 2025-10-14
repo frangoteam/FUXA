413954c5787019364e2f60fcdf793344189f4f8f
unocelli <unocelli@gmail.com>
2025-10-14 21:44:12 +0200

    refact: remove schema

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,6 @@
 // the start/root module that tells Angular how to assemble the application.
 
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -437,7 +437,6 @@ export const myCustomTooltipDefaults: MatTooltipDefaultOptions = {
         NgxDaterangepickerMd.forRoot(),
         FrameworkModule
     ],
-        schemas: [CUSTOM_ELEMENTS_SCHEMA],
     providers: [
         // providersResourceService,
         ResClientService,
