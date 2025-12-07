c149fbb6156402d7de1822f6072b4e7aa345f7de
unocelli <unocelli@gmail.com>
2025-12-07 22:41:23 +0100

    feat: backend apikey validation

--- server/api/index.js
+++ server/api/index.js
@@ -23,6 +23,7 @@ var schedulerApi = require('./scheduler');
 var commandApi = require('./command');
 const reports = require('../dist/reports.service');
 const reportsApi = new reports.ReportsApiService();
+const verifyApiOrToken = require('./apikeys/verify-api-or-token');
 
 var apiApp;
 var server;
@@ -41,31 +42,32 @@ function init(_server, _runtime) {
             apiApp.use(bodyParser.json({limit:maxApiRequestSize}));
             apiApp.use(bodyParser.urlencoded({limit:maxApiRequestSize, extended: true}));
             authJwt.init(runtime.settings.secureEnabled, runtime.settings.secretCode, runtime.settings.tokenExpiresIn);
-            prjApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            const authMiddleware = verifyApiOrToken(runtime);
+            prjApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(prjApi.app());
-            usersApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            usersApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(usersApi.app());
-            alarmsApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            alarmsApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(alarmsApi.app());
             authApi.init(runtime, authJwt.secretCode, authJwt.tokenExpiresIn);
             apiApp.use(authApi.app());
-            pluginsApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            pluginsApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(pluginsApi.app());
-            diagnoseApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            diagnoseApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(diagnoseApi.app());
-            daqApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            daqApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(daqApi.app());
-            schedulerApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            schedulerApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(schedulerApi.app());
-            scriptsApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            scriptsApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(scriptsApi.app());
-            resourcesApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            resourcesApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(resourcesApi.app());
-            commandApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            commandApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(commandApi.app());
-            reportsApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            reportsApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(reportsApi.app());
-            apiKeysApi.init(runtime, authJwt.verifyToken, verifyGroups);
+            apiKeysApi.init(runtime, authMiddleware, verifyGroups);
             apiApp.use(apiKeysApi.app());
 
             const limiter = rateLimit({
@@ -107,7 +109,7 @@ function init(_server, _runtime) {
             /**
              * POST Server user settings
              */
-            apiApp.post("/api/settings", authJwt.verifyToken, function(req, res, next) {
+            apiApp.post("/api/settings", authMiddleware, function(req, res, next) {
                 const permission = verifyGroups(req);
                 if (res.statusCode === 403) {
                     runtime.logger.error("api post settings: Tocken Expired");
@@ -134,7 +136,7 @@ function init(_server, _runtime) {
             /**
              * GET Heartbeat to check token
              */
-            apiApp.post('/api/heartbeat', authJwt.verifyToken, function (req, res) {
+            apiApp.post('/api/heartbeat', authMiddleware, function (req, res) {
                 if (!runtime.settings.secureEnabled) {
                     res.end();
                 } else if (res.statusCode === 403) {
@@ -189,6 +191,9 @@ function mergeUserSettings(settings) {
 
 function verifyGroups(req) {
     if (runtime.settings && runtime.settings.secureEnabled) {
+        if (req.apiKey) {
+            return authJwt.adminGroups[0];
+        }
         if (req.tokenExpired) {
             return (runtime.settings.userRole) ? null : 0;
         }
