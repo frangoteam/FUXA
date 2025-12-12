6fa1485a84d16eff191e0cc85b07ee4caeb0a380
unocelli <unocelli@gmail.com>
2025-12-12 12:12:42 +0100

    fix: update device values undefined

--- server/runtime/index.js
+++ server/runtime/index.js
@@ -159,7 +159,7 @@ function init(_io, _api, _settings, _log, eventsMain) {
                 if (message === 'get') {
                     var adevs = devices.getDevicesValues();
                     for (var id in adevs) {
-                        updateDeviceValues({ id: id, values: adevs[id] });
+                        updateDeviceValues({ id: id, values: adevs[id] || {} });
                     }
                 } else if (message.cmd === 'set' && message.var) {
                     devices.setDeviceValue(message.var.source, message.var.id, message.var.value, message.fnc);
@@ -319,7 +319,7 @@ function init(_io, _api, _settings, _log, eventsMain) {
                 if (message.sendLastValue) {
                     var adevs = devices.getDevicesValues();
                     for (var id in adevs) {
-                        updateDeviceValues({ id: id, values: adevs[id] });
+                        updateDeviceValues({ id: id, values: adevs[id] || {}});
                     }
                 }
             } catch (err) {
