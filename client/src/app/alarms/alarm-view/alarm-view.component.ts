36f8893df0ba9c9f631c87fba3178792ad1aa8e7
unocelli <unocelli@gmail.com>
2026-01-04 18:02:54 +0100

    fix: alarms

--- client/src/app/alarms/alarm-view/alarm-view.component.ts
+++ client/src/app/alarms/alarm-view/alarm-view.component.ts
@@ -18,7 +18,7 @@ import { LanguageService } from '../../_services/language.service';
 @Component({
     selector: 'app-alarm-view',
     templateUrl: './alarm-view.component.html',
-    styleUrls: ['./alarm-view.component.css']
+    styleUrls: ['./alarm-view.component.scss']
 })
 export class AlarmViewComponent implements OnInit, AfterViewInit, OnDestroy {
 
