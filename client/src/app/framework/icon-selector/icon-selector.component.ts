70018c89464edf2cd136ea663991721796b13ce8
unocelli <unocelli@gmail.com>
2026-02-19 21:53:58 +0100

    refactor: icon selector as framework component

---
+++ client/src/app/framework/icon-selector/icon-selector.component.ts
@@ -0,0 +1,51 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { BehaviorSubject, Observable, combineLatest, map, of } from 'rxjs';
+import { Define } from '../../_helpers/define';
+
+@Component({
+    selector: 'app-icon-selector',
+    templateUrl: './icon-selector.component.html',
+    styleUrls: ['./icon-selector.component.scss']
+})
+export class IconSelectorComponent implements OnInit {
+    @Input() value: string;
+    @Output() valueChange = new EventEmitter<string>();
+    @Output() selected = new EventEmitter<string>();
+
+    @Input() panelWidth = '310px';
+    @Input() filterPlaceholder = 'dlg.headeritem-icons-filter';
+    @Input() width = '60px';
+    @Input() height = '30px';
+
+    icons$: Observable<string[]>;
+    filteredIcons$: Observable<string[]>;
+    filterText = '';
+    private filterTextSubject = new BehaviorSubject<string>('');
+
+    ngOnInit() {
+        this.icons$ = of(Define.MaterialIconsRegular).pipe(
+            map((data: string) => data.split('\n')),
+            map(lines => lines.map(line => line.split(' ')[0])),
+            map(names => names.filter(name => !!name))
+        );
+
+        this.filteredIcons$ = combineLatest([
+            this.icons$,
+            this.filterTextSubject.asObservable()
+        ]).pipe(
+            map(([icons, filterText]) =>
+                icons.filter(icon => icon.toLowerCase().includes(filterText.toLowerCase()))
+            )
+        );
+    }
+
+    onFilterChange() {
+        this.filterTextSubject.next(this.filterText);
+    }
+
+    onSelect(icon: string) {
+        this.value = icon;
+        this.valueChange.emit(icon);
+        this.selected.emit(icon);
+    }
+}
