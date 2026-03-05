93afc5f983df18848e60147eb279bdaddfc530c3
unocelli <unocelli@gmail.com>
2026-03-05 20:19:10 +0100

    WIP

---
+++ client/src/app/editor/onboarding-wizard/onboarding-wizard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onboarding-wizard',
+  templateUrl: './onboarding-wizard.component.html',
+  styleUrls: ['./onboarding-wizard.component.scss']
+})
+export class OnboardingWizardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
