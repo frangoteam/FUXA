6967e9b35480d75160d010c60afd42860bf68672
unocelli <unocelli@gmail.com>
2026-03-20 08:44:55 +0100

    WIP

--- client/src/app/editor/onboarding-wizard/onboarding-wizard.component.ts
+++ client/src/app/editor/onboarding-wizard/onboarding-wizard.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-onboarding-wizard',
-  templateUrl: './onboarding-wizard.component.html',
-  styleUrls: ['./onboarding-wizard.component.scss']
+    selector: 'app-onboarding-wizard',
+    templateUrl: './onboarding-wizard.component.html',
+    styleUrls: ['./onboarding-wizard.component.scss']
 })
 export class OnboardingWizardComponent implements OnInit {
 
-  constructor() { }
+    formGroup: UntypedFormGroup;
 
-  ngOnInit() {
-  }
+    constructor(
+        public dialogRef: MatDialogRef<OnboardingWizardComponent>,
+        private fb: UntypedFormBuilder,
+    ) { }
 
+    ngOnInit() {
+        console.log('onboarding wizard init');
+    }
+
+    onNoClick(): void {
+        this.dialogRef.close();
+    }
+
+    onOkClick(): void {
+        this.dialogRef.close();
+    }
 }
