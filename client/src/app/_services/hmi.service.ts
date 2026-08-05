000b8f472e2e43cc92dc9181ed98f5528b4f14d1
unocelli <unocelli@gmail.com>
2026-08-05 22:54:51 +0200

    fix: derive socket.io origin from API endpoint, build

--- client/src/app/_services/hmi.service.ts
+++ client/src/app/_services/hmi.service.ts
@@ -195,8 +195,10 @@ export class HmiService {
         this.socket?.close();
         this.socketToken = normalizedToken;
         const query = normalizedToken ? { token: normalizedToken } : {};
-        this.socket = io(location.origin, {
-            path: EndPointApi.getBasePath() + '/socket.io',
+        const socketUrl = new URL(this.endPointConfig, location.origin);
+        const socketPath = socketUrl.pathname.replace(/\/+$/, '') + '/socket.io';
+        this.socket = io(socketUrl.origin, {
+            path: socketPath,
             query
         });
         this.socket.on('connect', () => {
